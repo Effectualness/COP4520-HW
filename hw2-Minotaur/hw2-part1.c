1e9cb46699d13ecd4da96fc067d72562f7b61cac
Marco Portocarrero <77308615+Effectualness@users.noreply.github.com>
2022-02-22 20:54:24 -0500

    Implemented hw2 windows implementation

---
+++ hw2-Minotaur/hw2-part1.c
@@ -0,0 +1,198 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+HANDLE* semArray;
+int cupcake = 1, alert = 0, numGuests;
+
+DWORD Leader(void* param)
+{
+    int guestNum = (int)param, count = 0;
+    srand(GetCurrentThreadId());
+    while(TRUE)
+    {
+        WaitForSingleObject(semArray[guestNum], INFINITE);
+        if(cupcake)
+        {
+            printf("Guest %d found a cupcake and ate it\n", guestNum);
+            cupcake = 0;
+            if(++count == numGuests)
+            {
+                alert = 1;
+                printf("Guest %d will alert the minotaur that all guests have entered the labyrinth at least once\n");
+                ReleaseSemaphore(semArray[0], 1, NULL);
+                return 1;
+            }
+        }
+        else
+        {
+            printf("Guest %d did not find a cupcake", guestNum);
+            if(rand() % 2)
+            {
+                printf(", requested another, and ate it\n", guestNum);
+            }
+            else
+            {
+                printf("and chose not to request another\n", guestNum);
+            }
+        }
+        ReleaseSemaphore(semArray[0], 1, NULL);
+    }
+}
+
+DWORD Guest(void* param)
+{
+    int guestNum = (int)param, firstEmpty = 1;
+    srand(GetCurrentThreadId());
+    while(TRUE)
+    {
+        WaitForSingleObject(semArray[guestNum], INFINITE);
+        if(alert)
+        {
+            return 1;
+        }
+        if(cupcake)
+        {
+            printf("Guest %d found a cupcake and did not eat it\n", guestNum);
+        }
+        else
+        {
+            printf("Guest %d did not find a cupcake", guestNum);
+            if(firstEmpty)
+            {
+                
+                printf(", requested another, and did not eat it\n", guestNum);
+                firstEmpty = 0;
+                cupcake = 1;
+            }
+            else
+            {
+                if(rand() % 2)
+                {
+                    printf(", requested another, and ate it\n", guestNum);
+                }
+                else
+                {
+                    printf("and chose not to request another\n", guestNum);
+                }
+            }
+        }
+        ReleaseSemaphore(semArray[0], 1, NULL);
+    }
+}
+
+DWORD Minotaur(void* param)
+{
+    int num;
+    srand(GetCurrentThreadId());
+    while(TRUE)
+    {
+        num = rand() % numGuests + 1;
+        printf("Minotaur has chosen guest %d to enter the labyrinth\n", num);
+        ReleaseSemaphore(semArray[num], 1, NULL);
+        WaitForSingleObject(semArray[0], INFINITE);
+        if(alert)
+        {
+            printf("The minotaur is pleased!\n");
+            for(num=0; num<numGuests; num++)
+            {
+                ReleaseSemaphore(semArray[num+1], 1, NULL);
+            }
+            return 1;
+        }
+    }
+}
+
+void GetUserInput()
+{
+    int i, total = 0;
+    char input[1024];
+    printf("How many guests will be particpating in the game? Enter an integer: ");
+    while(TRUE)
+    {
+        i = 0;
+        scanf("%s", input);
+        while(input[i] > 47 && input[i] < 58)
+        {
+            i++;
+        }
+        if(input[i] == 0)
+        {
+            i = 0;
+            while(input[i] != 0)
+            {
+                total = total * 10 + ((int)input[i] - 48);
+                i++;
+            }
+            if(total >= 10)
+            {
+                printf("The will create %d threads. Are you sure? (y/n): ", numGuests);
+                while(input[0] != 'n')
+                {
+                    scanf("%s", input);
+                    if(input[0] == 'y')
+                    {
+                        numGuests = total;
+                        return;
+                    }
+                    else if(input[0] == 'n')
+                    {
+                        total = 0;
+                        break;
+                    }
+                    else
+                    {
+                        printf("(y/n): ");
+                    }
+                }
+            }
+            else if(total == 0 || total == 1)
+            {
+                total = 0;
+                printf("There must be at least two guests to play the game\n");
+            }
+            else
+            {
+                numGuests = total;
+                return;
+            }
+        }
+        printf("Please enter a valid integer: ");
+    }
+}
+
+int main()
+{
+    GetUserInput();
+    srand(GetCurrentThreadId());
+    int i, leaderNum = rand()%numGuests+1;
+    semArray = (HANDLE*)malloc(sizeof(HANDLE) * (numGuests+1));
+    HANDLE* threadArray = (HANDLE*)malloc(sizeof(HANDLE) * (numGuests+1));
+    printf("\n%d guests are attending the party\nGuest %d was appointed the leader during the guests's planning period\n\n", numGuests, leaderNum);
+    for(i=0; i<numGuests+1; i++)
+    {
+        semArray[i] = CreateSemaphoreA(NULL, 0, 1, NULL);
+        if(i == 0)
+        {
+            threadArray[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Minotaur, NULL, NULL, NULL);
+        }
+        else if(i != leaderNum)
+        {
+            threadArray[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Guest, (int)i, NULL, NULL);
+        }
+        else
+        {
+            threadArray[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Leader, (int)i, NULL, NULL);
+        }
+    }
+    for(i=0; i<numGuests+1; i++)
+    {
+        WaitForSingleObject(threadArray[i], INFINITE);
+        CloseHandle(threadArray[i]);
+        CloseHandle(semArray[i]);
+    }
+    free(threadArray);
+    free(semArray);
+    printf("\nThe game has officially ended. Exiting...\n");
+    return 1;
+}
---
+++ hw2-Minotaur/hw2-part2.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+HANDLE showroom, print;
+int numGuests;
+
+DWORD Guest(void* param)
+{
+    int guestNum = (int)param, wait;
+    srand(GetCurrentThreadId());
+    while(TRUE)
+    {
+        wait = rand() % 6 * 1000;
+        printf("Guest %d will wander around for %d seconds\n", guestNum, wait/1000);
+        Sleep(wait);
+        WaitForSingleObject(print, INFINITE);
+        if(WaitForSingleObject(showroom, 0) == WAIT_OBJECT_0)
+        {
+            wait = rand() % 6 * 1000;
+            printf("Guest %d has entered the showroom, set BUSY sign, and will stay for %d seconds\n", guestNum, wait/1000);
+            ReleaseSemaphore(print, 1, NULL);
+            Sleep(wait);
+            WaitForSingleObject(print, INFINITE);
+            printf("Guest %d has left the showroom and set AVAILABLE sign\n", guestNum);
+            ReleaseSemaphore(showroom, 1, NULL);
+            ReleaseSemaphore(print, 1, NULL);
+            return 1;
+        }
+        else
+        {
+            printf("Guest %d could not go to the showroom as it was in use\n", guestNum);
+            ReleaseSemaphore(print, 1, NULL);
+        }
+    }
+    return 1;
+}
+
+void GetUserInput()
+{
+    int i, total = 0;
+    char input[1024];
+    printf("How many guests will be visiting the showroom? Enter an integer: ");
+    while(TRUE)
+    {
+        i = 0;
+        scanf("%s", input);
+        while(input[i] > 47 && input[i] < 58)
+        {
+            i++;
+        }
+        if(input[i] == 0)
+        {
+            i = 0;
+            while(input[i] != 0)
+            {
+                total = total * 10 + ((int)input[i] - 48);
+                i++;
+            }
+            if(total >= 100)
+            {
+                printf("%d guests may take a significant amount of time to complete. Are you sure? (y/n): ", numGuests);
+                while(input[0] != 'n')
+                {
+                    scanf("%s", input);
+                    if(input[0] == 'y')
+                    {
+                        numGuests = total;
+                        return;
+                    }
+                    else if(input[0] == 'n')
+                    {
+                        total = 0;
+                        break;
+                    }
+                    else
+                    {
+                        printf("(y/n): ");
+                    }
+                }
+            }
+            else
+            {
+                numGuests = total;
+                return;
+            }
+        }
+        printf("Please enter a valid integer: ");
+    }
+}
+
+int main()
+{
+    GetUserInput();
+    int i;
+    showroom = CreateSemaphoreA(NULL, 1, 1, NULL);
+    print = CreateSemaphoreA(NULL, 1, 1, NULL);
+    HANDLE* threadArray = (HANDLE*)malloc(sizeof(HANDLE) * numGuests);
+    for(i=0; i<numGuests; i++)
+    {
+        threadArray[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Guest, (int)i, NULL, NULL);
+    }
+    for(i=0; i<numGuests; i++)
+    {
+        WaitForSingleObject(threadArray[i], INFINITE);
+        CloseHandle(threadArray[i]);
+    }
+    CloseHandle(showroom);
+    CloseHandle(print);
+    free(threadArray);
+    printf("\nAll guests have attended the showroom. Exiting...\n");
+    return 0;
+}
